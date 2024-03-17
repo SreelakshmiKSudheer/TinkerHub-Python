0c38389566e9a25c4f5b8abb591e3a23aa9ab768
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-17 08:57:13 +0530

    2.7: Three Circles touching at the Bottom

---
+++ 2.7_Three_Circles_touching_at_the_Bottom.py
@@ -0,0 +1,12 @@
+#2.7: Three Circles touching at the Bottom
+
+from joy import *
+
+# create the inner most circle
+c1 = circle(x=0, y=25, r=25)
+
+# create the remaining two circles
+c2 = circle(x=0, y=50, r=50)
+c3 = circle(x=0, y=75, r=75)
+# show the circles
+show(c1,c2,c3)
