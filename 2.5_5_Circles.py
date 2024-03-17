ad120493e27131029bd857751463b2a54e38017c
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-17 08:51:34 +0530

    create 5 circle

---
+++ 2.5_5_Circles.py
@@ -0,0 +1,19 @@
+from joy import *
+
+#left
+c1 = circle(x=-100, y=0, r=50)
+
+#right
+c2 = circle(x=100, y=0, r=50)
+
+#top
+c3 = circle(x=0, y=100, r=50)
+
+#bottom
+c4 = circle(x=0, y=-100, r=50)
+
+#center
+c5 = circle(x=0, y=0, r=50)
+
+# show the circles
+show(c1,c2,c3,c4,c5)
