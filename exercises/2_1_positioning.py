3bed622c74b0ee918e4943b95773cf1ed20ba4e1
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-17 08:39:07 +0530

    3 circles in column

--- exercises/2_1_positioning.py
+++ exercises/2_1_positioning.py
@@ -1,7 +1,18 @@
 from joy import *
 
+'''
 # 3 circles in a row
 c1 = circle(x=-100, y=0, r=50)
 c2 = circle(x=0, y=0, r=50)
 c3 = circle(x=100, y=0, r=50)
+show(c1,c2,c3)
+'''
+# create the center circle
+c1 = circle(x=0, y=0, r=50)
+
+# create the top and the bottom
+c2 = circle(x=0, y=100, r=50)
+c3 = circle(x=0, y=-100, r=50)
+
+# show the circles
 show(c1,c2,c3)
