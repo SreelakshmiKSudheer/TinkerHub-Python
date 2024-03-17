6477bacd244eb67657fac4412ab58fa8a6eb0312
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-17 08:35:42 +0530

    3 circles in row

--- exercises/2_1_positioning.py
+++ exercises/2_1_positioning.py
@@ -1,6 +1,7 @@
 from joy import *
 
-# circle positioning
-c1 = circle(x=-50, y=0, r=50)
-c2 = circle(x=50, y=0, r=50)
-show(c1,c2)
+# 3 circles in a row
+c1 = circle(x=-100, y=0, r=50)
+c2 = circle(x=0, y=0, r=50)
+c3 = circle(x=100, y=0, r=50)
+show(c1,c2,c3)
