1dc41cec4397303d2db8991b616923bbec5fb8bb
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-17 09:00:57 +0530

    2.8: Three large Circles touching at the Bottom

--- 2.7_Three_Circles_touching_at_the_Bottom.py
+++ 2.7_Three_Circles_touching_at_the_Bottom.py
@@ -1,12 +1,24 @@
 #2.7: Three Circles touching at the Bottom
+#2.8: Three Large Circles touching at the Bottom
 
 from joy import *
 
+'''
 # create the inner most circle
 c1 = circle(x=0, y=25, r=25)
 
 # create the remaining two circles
 c2 = circle(x=0, y=50, r=50)
 c3 = circle(x=0, y=75, r=75)
+# show the circles
+show(c1,c2,c3)
+'''
+
+# create the inner most circle
+c1 = circle(x=0, y=-100, r=50)
+
+# create the remaining two circles
+c2 = circle(x=0, y=-50, r=100)
+c3 = circle(x=0, y=0, r=150)
 # show the circles
 show(c1,c2,c3)
