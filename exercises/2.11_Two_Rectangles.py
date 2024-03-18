87039f50df554950d4ada8a25939b46a094cd663
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-18 05:46:27 +0530

    Two Rectangles

---
+++ exercises/2.11_Two_Rectangles.py
@@ -0,0 +1,10 @@
+from joy import *
+
+# the first rectangle
+r1 = rectangle(w=200, h=100)
+
+# create second rectangle r2
+r2 = rectangle(w=100, h=200)
+
+# change this to show both the shapes
+show(r1,r2)
