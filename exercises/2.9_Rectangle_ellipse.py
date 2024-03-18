24a873a6390c4e4a964095bd35b86b8605f2ce63
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-18 05:39:16 +0530

    Square

--- exercises/2.9_Rectangle_ellipse.py
+++ exercises/2.9_Rectangle_ellipse.py
@@ -1,7 +1,10 @@
 from joy import *
 
 # draw rectangle and ellipse with specified dimension
+'''
 r1 = rectangle(w = 200, h = 100)
 e1 = ellipse(w = 150, h = 100)
-
-show(r1,e1)
+'''
+# Square
+s = rectangle(x = 0, y = 0, w = 200, h = 200)
+show(s)
