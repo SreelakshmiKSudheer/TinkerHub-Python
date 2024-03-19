817377eb33265135ad8a281959c1a158728b3aed
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-19 07:07:47 +0530

    hash

---
+++ exercises/2.15_Hash.py
@@ -0,0 +1,21 @@
+from joy import *
+
+# your code here
+x1,y1 = -50,100
+x2,y2 = -50,-100
+
+x3,y3 = 50,100
+x4,y4 = 50,-100
+
+x5,y5 = 100,50
+x6,y6 = -100,50
+
+x7,y7 = 100,-50
+x8,y8 = -100,-50
+
+l1 = line(x1,y1,x2,y2)
+l2 = line(x3,y3,x4,y4)
+l3 = line(x5,y5,x6,y6)
+l4 = line(x7,y7,x8,y8)
+
+show(l1,l2,l3,l4)
