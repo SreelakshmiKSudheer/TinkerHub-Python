06d273e7faeb4c9ad7be5a39db094a6ab5cbed3e
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-20 06:04:30 +0530

    Update no stroke style

--- exercises/BasicStyles/Stroke_Color.py
+++ exercises/BasicStyles/Stroke_Color.py
@@ -1,4 +1,9 @@
 from joy import *
 
-r = rectangle(stroke = "red")
-show(r)
+#r = rectangle(fill = "yellow", stroke = "red")
+#show(r)
+
+c1 = circle(r = 150, fill = 'green', stroke = "none")
+c2 = circle(r = 100, fill = 'blue', stroke = "none")
+c3 = circle(r = 50, fill = 'red', stroke = "none")
+show(c1,c2,c3)
