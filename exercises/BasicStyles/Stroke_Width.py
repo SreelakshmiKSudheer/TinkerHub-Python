bfc0155a3890791f88fd740112d40dd95f8bccb0
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-20 06:08:33 +0530

    Update stroke width

---
+++ exercises/BasicStyles/Stroke_Width.py
@@ -0,0 +1,9 @@
+from joy import *
+
+c1 = circle(r = 25, stroke_width = 1)
+c2 = circle(r = 50, stroke_width = 2)
+c3 = circle(r = 75, stroke_width = 2)
+c4 = circle(r = 100, stroke_width = 4)
+c5 = circle(r = 125, stroke_width = 5)
+
+show(c1,c2,c3)
