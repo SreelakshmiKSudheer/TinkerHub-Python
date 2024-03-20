92e42b172c10efe7bebaef95bbd9b2a75330adb3
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-20 06:19:41 +0530

    Update

---
+++ exercises/BasicStyles/Three_overlappin_squares.py
@@ -0,0 +1,7 @@
+from joy import *
+
+r1 = rectangle(x = -50, y = -50, w = 100, h = 100, fill = 'green', stroke = 'none')
+r2 = rectangle(w = 100, h = 100, fill = 'red', stroke = 'none')
+r3 = rectangle(x = 50, y = 50, w = 100, h = 100, fill = 'blue', stroke = 'none')
+
+show(r1,r2,r3)
