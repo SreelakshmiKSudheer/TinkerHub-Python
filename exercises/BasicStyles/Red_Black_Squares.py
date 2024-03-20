97b3de12469fe57f8117313f45eced493f625db6
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-20 06:14:11 +0530

    Update

---
+++ exercises/BasicStyles/Red_Black_Squares.py
@@ -0,0 +1,7 @@
+from joy import *
+
+r1 = rectangle(x = -75, y = 75, w = 150, h = 150, fill = 'red')
+r2 = rectangle(x = 75, y = 75, w = 150, h = 150, fill = 'black')
+r3 = rectangle(x = -75, y = -75, w = 150, h = 150, fill = 'black')
+r4 = rectangle(x = 75, y = -75, w = 150, h = 150, fill = 'red')
+show(r1,r2,r3,r4)
