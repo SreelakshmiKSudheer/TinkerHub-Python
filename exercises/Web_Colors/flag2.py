ab89c5cc196c3dc10ab1df648691c5884142c63e
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-28 21:11:46 +0530

    flag 2

---
+++ exercises/Web_Colors/flag2.py
@@ -0,0 +1,6 @@
+from joy import *
+
+r1 = rectangle(x=-100, y=0, h=300, w=100, fill='#008c45', stroke='none')        # fren green
+r2 = rectangle(x=0, y=0, h=300, w=100, fill='#f4f5f0', stroke='none')           # bright white
+r3 = rectangle(x=100, y=0, h=300, w=100, fill='#cd212a', stroke='none')         # flame scarlet
+show(r1, r2, r3)
