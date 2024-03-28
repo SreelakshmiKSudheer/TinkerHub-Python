fe2acf65355c0bdd3c12724d68bc62d69b6ea0f1
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-28 21:05:18 +0530

    flag 1

---
+++ exercises/Web_Colors/flag1.py
@@ -0,0 +1,6 @@
+from joy import *
+
+r1 = rectangle(x=0, y=100, w=300, h=100, fill='black', stroke='none')
+r2 = rectangle(x=0, y=0, w=300, h=100, fill='red', stroke='none')
+r3 = rectangle(x=0, y=-100, w=300, h=100, fill='gold', stroke='none')
+show(r1, r2, r3)
