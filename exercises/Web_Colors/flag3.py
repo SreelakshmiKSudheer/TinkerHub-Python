d97a4f328aad764d72951a54a6f3cde8397c262e
SreelakshmiKSudheer <sreelakshmikavenge3i3@gmail.com>
2024-03-28 21:15:07 +0530

    flag 3

---
+++ exercises/Web_Colors/flag3.py
@@ -0,0 +1,6 @@
+from joy import *
+
+r1 = rectangle(x=0, y=100, w=300, h=100, fill='#ae1c28', stroke='none')     # Bright vermilion
+r2 = rectangle(x=0, y=0, w=300, h=100, fill='#ffffff', stroke='none')       # White
+r3 = rectangle(x=0, y=-100, w=300, h=100, fill='#21468b', stroke='none')    # Cobalt Blue
+show(r1, r2, r3)
